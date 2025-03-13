464a91381aca5c9fefffae0a686c733086ce2c16
Merkava3 <lennzoferrari@hotmail.com>
2025-03-13 17:42:19 -0500

    fix(config): correct import in config.py

--- api/config.py
+++ api/config.py
@@ -6,7 +6,7 @@ class DevelopmentConfig(Config):
     #'postgresql://postgres:Dimichev3.@localhost:5432/tecnoexpress'
     #postgresql://tecnoexpress_user:z4V8ZmwzObW0J9odCXY69rFMYFKVGIoz@dpg-cv3ngj7noe9s738n58qg-a.oregon-postgres.render.com/tecnoexpress
     # internal connection : postgresql://tecnoexpress_user:z4V8ZmwzObW0J9odCXY69rFMYFKVGIoz@dpg-cv3ngj7noe9s738n58qg-a/tecnoexpress
-    SQLALCHEMY_DATABASE_URI = 'postgresql://tecnoexpress_user:z4V8ZmwzObW0J9odCXY69rFMYFKVGIoz@dpg-cv3ngj7noe9s738n58qg-a/tecnoexpress'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Dimichev3.@localhost:5432/tecnoexpress'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class TestConfig(Config):

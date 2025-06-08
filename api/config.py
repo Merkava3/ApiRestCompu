6b08a3d0ac1e2f3ecb65ba26024ca3ad8099d265
Merkava3 <lennzoferrari@hotmail.com>
2025-06-07 20:43:50 -0500

    config(conection) update configuration database(#96)

--- api/config.py
+++ api/config.py
@@ -6,7 +6,7 @@ class DevelopmentConfig(Config):
     #'postgresql://postgres:Dimichev3.@localhost:5432/tecnoexpress'
     #postgresql://tecnoexpress_user:z4V8ZmwzObW0J9odCXY69rFMYFKVGIoz@dpg-cv3ngj7noe9s738n58qg-a.oregon-postgres.render.com/tecnoexpress
     # internal connection : postgresql://tecnoexpress_t9ja_user:NvvQL6uRAGCdwzNPzYSFJhgO9TIVQThX@dpg-cvnvbo3e5dus73e1t47g-a/tecnoexpress_t9ja
-    SQLALCHEMY_DATABASE_URI = 'postgresql://tecnoexpress_80td_user:wC2qhBfvbZjMxBCIo0VN7tvQpBYIuon9@dpg-d0cdfvc9c44c73drnvf0-a.oregon-postgres.render.com/tecnoexpress_80td'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://tecnoexpress_vunw_user:YzqRbUB1TN3NvAu2rFKRtOHNrRrdafw0@dpg-d11qnfemcj7s73el20mg-a.oregon-postgres.render.com/tecnoexpress_vunw'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class TestConfig(Config):

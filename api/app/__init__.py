6b02b24651c7c2d1b8856a0a6e4b3ca80cc423f2
Merkava3 <lennzoferrari@hotmail.com>
2025-06-16 22:52:21 -0500

    chore(api): enable CORS and update dependencies (#98)

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from .models import db
 from .views import api_v1
 from api.config import config
+from flask_cors import CORS 
 
 def create_app(enviroment):
     app = Flask(__name__)
-    app.config.from_object(enviroment) 
+    app.config.from_object(enviroment)
+    CORS(app)
     if 'api_v1' not in app.blueprints:
         app.register_blueprint(api_v1, url_prefix='/api/v1')
     with app.app_context():

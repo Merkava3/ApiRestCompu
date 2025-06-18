34f4c22bc62471cea2fddf0ab4f3d925be7a4496
Merkava3 <lennzoferrari@hotmail.com>
2025-06-18 11:57:46 -0500

    chore(api): enable CORS and update dependencies

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -11,7 +11,6 @@ def create_app(enviroment):
     if 'api_v1' not in app.blueprints:
         app.register_blueprint(api_v1, url_prefix='/api/v1')
     with app.app_context():
-        db.init_app(app)
-        #print("DATABASE URI:", app.config.get("SQLALCHEMY_DATABASE_URI"))       
+        db.init_app(app)            
         db.create_all()
     return app

2fb6f10aa1f3938a285bc5aed2c008512a760ebb
lszondas <lszondas@gmail.com>
2025-06-21 15:46:59 +0000

    chore(config) change link conection neo postgresql

--- api/config.py
+++ api/config.py
@@ -6,7 +6,11 @@ class DevelopmentConfig(Config):
     #'postgresql://postgres:Dimichev3.@localhost:5432/tecnoexpress'
     #postgresql://tecnoexpress_user:z4V8ZmwzObW0J9odCXY69rFMYFKVGIoz@dpg-cv3ngj7noe9s738n58qg-a.oregon-postgres.render.com/tecnoexpress
     # internal connection : postgresql://tecnoexpress_t9ja_user:NvvQL6uRAGCdwzNPzYSFJhgO9TIVQThX@dpg-cvnvbo3e5dus73e1t47g-a/tecnoexpress_t9ja
+<<<<<<< HEAD
     SQLALCHEMY_DATABASE_URI = 'postgresql://tecnoexpress_vunw_user:YzqRbUB1TN3NvAu2rFKRtOHNrRrdafw0@dpg-d11qnfemcj7s73el20mg-a.oregon-postgres.render.com/tecnoexpress_vunw'
+=======
+    SQLALCHEMY_DATABASE_URI = 'postgresql://tecnoexpress_owner:npg_zNdIya8tj6TG@ep-round-paper-acxjrr4q-pooler.sa-east-1.aws.neon.tech/tecnoexpress?sslmode=require'
+>>>>>>> 8f0b6e2 (chore(config) change link conection neo postgresql)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class TestConfig(Config):

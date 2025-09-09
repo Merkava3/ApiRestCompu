d3c804bcdff0f9b7d533b9257036e9fb9dcdbb34
Merkava3 <lennzoferrari@hotmail.com>
2025-09-09 15:11:38 -0500

    feat(services) update endpoint search

--- api/app/routers/servicios_routers.py
+++ api/app/routers/servicios_routers.py
@@ -40,9 +40,10 @@ def post_client():
         return response(api_servicio.dump(servicio))    
     return badRequest()
 
-@servicios_routes.route('/search/servicio', methods=['POST'])
+@servicios_routes.route('/search/servicio', methods=['GET'])
 @set_servicios_by()
 def get_servicio(servicio):
+    print("estamos aca buscar servicio")
     return response(api_servicio.dump(servicio))
 
 @servicios_routes.route('/servicio', methods=['PUT'])

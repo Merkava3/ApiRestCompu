826ed83ed7bb4f3fa9aaebd558bba4bbef83a273
Merkava3 <lennzoferrari@hotmail.com>
2026-01-24 15:17:50 -0500

    fix: use dynamic port from environment variable

--- api/main.py
+++ api/main.py
@@ -28,6 +28,6 @@ if __name__ == "__main__":
     socketio.run(
         app, 
         host="0.0.0.0", 
-        port=5000, 
+        port=int(os.environ.get("PORT", 5000)), 
         debug=app.config.get('DEBUG', False)
     )
